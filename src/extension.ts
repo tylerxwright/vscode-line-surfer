ba8bfd59fe0962b54493ecab935cdd60eb60c1d8
Tyler Wright <tylerxwright@gmail.com>
2021-05-24 19:33:06 -0400

    Switched over to a class structure and I also added configurations

--- src/extension.ts
+++ src/extension.ts
@@ -1,100 +1,121 @@
 import * as vscode from 'vscode';
 import { DecorationRenderOptions, TextEditor, TextEditorDecorationType, TextEditorSelectionChangeEvent } from 'vscode';
-import * as interpolate from 'color-interpolate';
+import interpolate from 'color-interpolate';
 
-interface Wave {
-    above: TextEditorDecorationType[];
-    center: TextEditorDecorationType;
-    below: TextEditorDecorationType[];
+class Wave {
+    above: TextEditorDecorationType[] = [];
+    below: TextEditorDecorationType[] = [];
+    center?: TextEditorDecorationType;
+    colors: string[] = [];
+    amplitude: number = 3;
+    
+    colormaps?: (index: number) => string;
+
+    constructor(context: vscode.ExtensionContext) {
+        this.reload();
+
+        let disposable = vscode.commands.registerCommand('line-surfer.surf', () => {
+            vscode.window.showInformationMessage('Surfs up!');
+            vscode.window.onDidChangeTextEditorSelection(
+                (evt: TextEditorSelectionChangeEvent) => this.createWave(
+                    evt.textEditor.selection.active.line, 
+                    evt.textEditor
+                )
+            );
+            vscode.workspace.onDidChangeConfiguration(
+                (evt: vscode.ConfigurationChangeEvent) => {
+                    if(evt.affectsConfiguration('lineSurfer')) {
+                        this.reload();
+                    }
+                }
+            );
+        });
+    
+        context.subscriptions.push(disposable);
+    
+        if(vscode.window.activeTextEditor !== undefined) {
+            this.createWave(vscode.window.activeTextEditor.selection.active.line, vscode.window.activeTextEditor);
+        }
+    }
+
+    reload() {
+        const config = vscode.workspace.getConfiguration('lineSurfer');
+
+        this.above.forEach((line) => {
+
+        });
+
+        this.center = undefined;
+        this.above = [];
+        this.below = [];
+
+        this.colors = [
+            config.get('crestColor') as string, 
+            config.get('troughColor') as string
+        ];
+
+        this.amplitude = Number(config.get('amplitude'));
+
+        this.colormaps = interpolate(this.colors);
+
+        this.initializeWaves();
+    }
+
+    initializeWaves() {
+        const centerWave = vscode.window.createTextEditorDecorationType(<DecorationRenderOptions> {
+            backgroundColor: this.getWaveColor(0),
+            fontWeight: '900',
+        });
+    
+        this.center = centerWave;
+    
+        for(let i=1; i<this.amplitude; i++) {
+            const aboveWave = vscode.window.createTextEditorDecorationType(<DecorationRenderOptions> {
+                backgroundColor: this.getWaveColor(i),
+            });
+    
+            const belowWave = vscode.window.createTextEditorDecorationType(<DecorationRenderOptions> {
+                backgroundColor: this.getWaveColor(i),
+            });
+    
+            this.above.push(aboveWave);
+            this.below.push(belowWave);
+        }
+    }
+
+    getWaveColor(index: number): string {
+        return this.colormaps === undefined ? '#000000' : this.colormaps(100 / this.amplitude * index / 100);
+    }
+
+    createWave(lineNumber: number, editor: TextEditor) {
+        this.decorateLine(lineNumber, 0, editor);
+    
+        for(let i=1; i<this.amplitude; i++) {
+            this.decorateLine(lineNumber, -i, editor);
+            this.decorateLine(lineNumber, i, editor);
+        }
+    }
+    
+    decorateLine(lineNumber: number, offset: number, editor: TextEditor) {
+        if(this.center === undefined) {
+            return;
+        }
+    
+        const line = editor.document.lineAt(lineNumber + offset);
+    
+        let waveDecoration: TextEditorDecorationType = this.center;
+        if(offset < 0) {
+            waveDecoration = this.above[Math.abs(offset) - 1];
+        } else if(offset > 0) {
+            waveDecoration = this.below[Math.abs(offset) - 1];
+        }
+    
+        editor.setDecorations(waveDecoration, [line]);
+    }
 }
-
-let wave: Wave;
-const colormap = interpolate(['#76398', '#872909']);
-const waveColorBase = 5;
-const waveOpacity = 1;
-const waveFontWeight = 900;
-const waveHeight = 4;
-const waveModifier = 20;
 
 export function activate(context: vscode.ExtensionContext) {
-    initializeWaves(waveHeight);
-
-    let disposable = vscode.commands.registerCommand('line-surfer.surf', () => {
-        vscode.window.showInformationMessage('Surfs up!');
-        vscode.window.onDidChangeTextEditorSelection((evt: TextEditorSelectionChangeEvent) => createWave(evt.textEditor.selection.active.line, evt.textEditor));
-    });
-
-    context.subscriptions.push(disposable);
-
-    if(vscode.window.activeTextEditor !== undefined) {
-        createWave(vscode.window.activeTextEditor.selection.active.line, vscode.window.activeTextEditor);
-    }
+    new Wave(context);
 }
 
-function initializeWaves(numberOfWaves: number) {
-    const centerWave = vscode.window.createTextEditorDecorationType(<DecorationRenderOptions> {
-        backgroundColor: getWaveColor(0),
-        opacity: waveOpacity.toString(),
-        fontWeight: waveFontWeight.toString(),
-    });
-
-    wave = {
-        above: [],
-        center: centerWave,
-        below: []
-    }
-
-    for(let i=1; i<numberOfWaves; i++) {
-        const aboveWave = vscode.window.createTextEditorDecorationType(<DecorationRenderOptions> {
-            backgroundColor: getWaveColor(i),
-            opacity: waveOpacity.toString(),
-        });
-
-        const belowWave = vscode.window.createTextEditorDecorationType(<DecorationRenderOptions> {
-            backgroundColor: getWaveColor(i),
-            opacity: waveOpacity.toString(),
-        });
-
-        wave.above.push(aboveWave);
-        wave.below.push(belowWave);
-    }
-}
-
-function getWaveColor(offset: number): string {
-    const colorValue = (waveColorBase - offset) * waveModifier;
-    return rgbToHex(colorValue, colorValue, colorValue);
-}
-
-function componentToHex(c: number): string {
-    var hex = c.toString(16);
-    return hex.length == 1 ? "0" + hex : hex;
-}
-  
-function rgbToHex(r: number, g: number, b: number): string {
-    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
-}
-
-function createWave(lineNumber: number, editor: TextEditor) {
-    decorateLine(lineNumber, 0, editor);
-
-    for(let i=1; i<waveHeight; i++) {
-        decorateLine(lineNumber, -i, editor);
-        decorateLine(lineNumber, i, editor);
-    }
-}
-
-function decorateLine(lineNumber: number, offset: number, editor: TextEditor) {
-    const line = editor.document.lineAt(lineNumber + offset);
-
-    let waveDecoration: TextEditorDecorationType = wave.center;
-    if(offset < 0) {
-        waveDecoration = wave.above[Math.abs(offset) - 1];
-    } else if(offset > 0) {
-        waveDecoration = wave.below[Math.abs(offset) - 1];
-    }
-
-    editor.setDecorations(waveDecoration, [line]);
-}
-
-// this method is called when your extension is deactivated
 export function deactivate() {}
