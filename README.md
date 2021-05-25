66fadd41a6cd8ae3fc34db1f07784d233ed0db12
Tyler Wright <tylerxwright@gmail.com>
2021-05-25 14:26:36 -0400

    Ready to publish

--- README.md
+++ README.md
@@ -1,70 +1,30 @@
-# line-surfer README
+# ![Line Surfer Logo](images/logo-small.png) Line Surfer - Surfs up! 
 
-This is the README for your extension "line-surfer". After writing up a brief description, we recommend including the following sections.
+Turn your cursor into a wave. Line Surfer is a highlighter that applies a gradient to the current line in the active editor that expands above and below.
+
+![Line Surfer Demo](images/line-surfer-demo.gif)
 
 ## Features
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
+- Provides a configurable reflected gradient centered on your current line in an active editor
+- Change the number of lines the gradient expands above and below
+- Easily identify your current line and those surrounding it
 
-For example if there is an image subfolder under your extension project workspace:
+## Settings
 
-\!\[feature X\]\(images/feature-x.png\)
-
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
-
-## Requirements
-
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
-
-## Extension Settings
-
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
-
-For example:
-
-This extension contributes the following settings:
-
-* `myExtension.enable`: enable/disable this extension
-* `myExtension.thing`: set to `blah` to do something
-
-## Known Issues
-
-Calling out known issues can help limit users opening duplicate issues against your extension.
+* `lineSurfer.crestColor`: The color of the current line in the active editor
+* `lineSurfer.troughColor`: The color that is interpolated out to based on the amplitude setting
+* `lineSurfer.amplitude`: The number of lines above and below that the gradient expands to
 
 ## Release Notes
 
-Users appreciate release notes as you update your extension.
-
 ### 1.0.0
 
-Initial release of ...
+Initial release.
+  
+- Current line gradient, aka "The Wave"
+- Settings to configure the size and colors of the wave.
 
-### 1.0.1
+----------------------------------------------------------------------------------------
 
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
-
------------------------------------------------------------------------------------------------------------
-## Following extension guidelines
-
-Ensure that you've read through the extensions guidelines and follow the best practices for creating your extension.
-
-* [Extension Guidelines](https://code.visualstudio.com/api/references/extension-guidelines)
-
-## Working with Markdown
-
-**Note:** You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux)
-* Toggle preview (`Shift+CMD+V` on macOS or `Shift+Ctrl+V` on Windows and Linux)
-* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (macOS) to see a list of Markdown snippets
-
-### For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-**Enjoy!**
+## **Get Rad!**
---
+++ src/wave.ts
@@ -0,0 +1,114 @@
+import * as vscode from 'vscode';
+import { DecorationRenderOptions, TextEditor, TextEditorDecorationType, TextEditorSelectionChangeEvent } from 'vscode';
+import interpolate from 'color-interpolate';
+
+export class Wave {
+    private above: TextEditorDecorationType[] = [];
+    private below: TextEditorDecorationType[] = [];
+    private center?: TextEditorDecorationType;
+    private colors: string[] = [];
+    private amplitude: number = 3;
+    
+    colormaps?: (index: number) => string;
+
+    constructor() {
+        this.reload();
+
+        vscode.window.onDidChangeTextEditorSelection(
+            (evt: TextEditorSelectionChangeEvent) => this.createWave(
+                evt.textEditor.selection.active.line, 
+                evt.textEditor
+            )
+        );
+        vscode.workspace.onDidChangeConfiguration(
+            (evt: vscode.ConfigurationChangeEvent) => {
+                if(evt.affectsConfiguration('lineSurfer')) {
+                    this.reload();
+                }
+            }
+        );
+    
+        if(vscode.window.activeTextEditor !== undefined) {
+            this.createWave(vscode.window.activeTextEditor.selection.active.line, vscode.window.activeTextEditor);
+        }
+    }
+
+    private reload() {
+        const config = vscode.workspace.getConfiguration('lineSurfer');
+
+        this.reset();
+        
+        this.colors = [
+            config.get('crestColor') as string, 
+            config.get('troughColor') as string
+        ];
+
+        this.amplitude = Number(config.get('amplitude'));
+
+        this.colormaps = interpolate(this.colors);
+
+        this.initializeWaves();
+    }
+
+    reset() {
+        this.above.forEach((lineDecoration) => lineDecoration.dispose());
+        this.below.forEach((lineDecoration) => lineDecoration.dispose());
+        this.center?.dispose();
+
+        this.center = undefined;
+        this.above = [];
+        this.below = [];
+    }
+
+    private initializeWaves() {
+        const centerWave = vscode.window.createTextEditorDecorationType(<DecorationRenderOptions> {
+            backgroundColor: this.getWaveColor(0),
+            fontWeight: '900',
+        });
+    
+        this.center = centerWave;
+    
+        for(let i=1; i<this.amplitude; i++) {
+            const aboveWave = vscode.window.createTextEditorDecorationType(<DecorationRenderOptions> {
+                backgroundColor: this.getWaveColor(i),
+            });
+    
+            const belowWave = vscode.window.createTextEditorDecorationType(<DecorationRenderOptions> {
+                backgroundColor: this.getWaveColor(i),
+            });
+    
+            this.above.push(aboveWave);
+            this.below.push(belowWave);
+        }
+    }
+
+    private getWaveColor(index: number): string {
+        return this.colormaps === undefined ? '#000000' : this.colormaps(100 / (this.amplitude - 1 ) * index / 100);
+    }
+
+    private createWave(lineNumber: number, editor: TextEditor) {
+        this.decorateLine(lineNumber, 0, editor);
+    
+        for(let i=1; i<this.amplitude; i++) {
+            this.decorateLine(lineNumber, -i, editor);
+            this.decorateLine(lineNumber, i, editor);
+        }
+    }
+    
+    private decorateLine(lineNumber: number, offset: number, editor: TextEditor) {
+        if(this.center === undefined) {
+            return;
+        }
+    
+        const line = editor.document.lineAt(lineNumber + offset);
+        
+        let waveDecoration = this.center;
+        if(offset < 0) {
+            waveDecoration = this.above[Math.abs(offset) - 1];
+        } else if(offset > 0) {
+            waveDecoration = this.below[Math.abs(offset) - 1];
+        }
+    
+        editor.setDecorations(waveDecoration, [line]);
+    }
+}
